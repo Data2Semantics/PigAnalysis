2a65b5f991ab7e49fb557f24ae397213bb619980
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-03-27 00:48:07 +0100

    refactored scripts

--- scripts.sh
+++ scripts.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#pig pigAnalysis/roundtrip/toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_litAsNode/directed_indegree dbp/roundtrip/dbp_s-o_unweighted_litAsNode/directed_indegree;
-pig pigAnalysis/roundtrip/toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_litAsNode/directed_outdegree dbp/roundtrip/dbp_s-o_unweighted_litAsNode/directed_outdegree;
-pig pigAnalysis/roundtrip/toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_litAsNode/directed_pagerank dbp/roundtrip/dbp_s-o_unweighted_litAsNode/directed_pagerank;
-pig pigAnalysis/roundtrip/toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_litAsNode/undirected_degree dbp/roundtrip/dbp_s-o_unweighted_litAsNode/undirected_degree;
-pig pigAnalysis/roundtrip/toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_litAsNode/undirected_pagerank dbp/roundtrip/dbp_s-o_unweighted_litAsNode/undirected_pagerank;
+#pig pigAnalysis/roundtrip/s-o_toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_litAsNode/directed_indegree dbp/roundtrip/dbp_s-o_unweighted_litAsNode/directed_indegree;
+pig pigAnalysis/roundtrip/s-o_toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_litAsNode/directed_outdegree dbp/roundtrip/dbp_s-o_unweighted_litAsNode/directed_outdegree;
+pig pigAnalysis/roundtrip/s-o_toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_litAsNode/directed_pagerank dbp/roundtrip/dbp_s-o_unweighted_litAsNode/directed_pagerank;
+pig pigAnalysis/roundtrip/s-o_toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_litAsNode/undirected_degree dbp/roundtrip/dbp_s-o_unweighted_litAsNode/undirected_degree;
+pig pigAnalysis/roundtrip/s-o_toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_litAsNode/undirected_pagerank dbp/roundtrip/dbp_s-o_unweighted_litAsNode/undirected_pagerank;
 
 
 
-pig pigAnalysis/roundtrip/toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_noLit/directed_indegree dbp/roundtrip/dbp_s-o_unweighted_noLit/directed_indegree;
-pig pigAnalysis/roundtrip/toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_noLit/directed_outdegree dbp/roundtrip/dbp_s-o_unweighted_noLit/directed_outdegree;
-pig pigAnalysis/roundtrip/toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_noLit/directed_pagerank dbp/roundtrip/dbp_s-o_unweighted_noLit/directed_pagerank;
-pig pigAnalysis/roundtrip/toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_noLit/undirected_degree dbp/roundtrip/dbp_s-o_unweighted_noLit/undirected_degree;
-pig pigAnalysis/roundtrip/toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_noLit/undirected_pagerank dbp/roundtrip/dbp_s-o_unweighted_noLit/undirected_pagerank;
+pig pigAnalysis/roundtrip/s-o_toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_noLit/directed_indegree dbp/roundtrip/dbp_s-o_unweighted_noLit/directed_indegree;
+pig pigAnalysis/roundtrip/s-o_toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_noLit/directed_outdegree dbp/roundtrip/dbp_s-o_unweighted_noLit/directed_outdegree;
+pig pigAnalysis/roundtrip/s-o_toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_noLit/directed_pagerank dbp/roundtrip/dbp_s-o_unweighted_noLit/directed_pagerank;
+pig pigAnalysis/roundtrip/s-o_toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_noLit/undirected_degree dbp/roundtrip/dbp_s-o_unweighted_noLit/undirected_degree;
+pig pigAnalysis/roundtrip/s-o_toTripleRankings.py dbp/dbp.nt dbp/analysis/dbp_s-o_unweighted_noLit/undirected_pagerank dbp/roundtrip/dbp_s-o_unweighted_noLit/undirected_pagerank;
 
 #pig pigAnalysis/roundtrip/selectTopK.py dbp/roundtrip/dbp_s-o_unweighted_litAsNode/directed_indegree 50;
 pig pigAnalysis/roundtrip/selectTopK.py dbp/roundtrip/dbp_s-o_unweighted_litAsNode/directed_outdegree 50;
