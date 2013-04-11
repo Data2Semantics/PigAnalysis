e484d165084f799242372ab6d1b2b21cc0cbca83
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-04-11 16:53:11 +0200

    initial implementation for adding dir to virtuoso

---
+++ bin/putDirInVirtuoso.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -z "$1" ];then
+	echo "at least 1 argument required (the directory to add to virtuoso)"
+	exit
+fi
+source /home/OpenPHACTS-Virtuoso/virtuoso-environment.sh;
+isqlFile="${HOME}/.isqlCmdFile.sql"
+dirPath=$1
+
+
+
+if [ ! -d $dirPath ]; then
+	echo "dir $dirPath does not exist"
+	exit;
+fi
+
+basename=`basename $dirPath`
+graphname="http://$basename"
+
+echo "CLEAR GRAPH <$graphname>" > $isqlFile;
+echo "" >> $isqlFile;
+cat $isqlFile | isql;
+
