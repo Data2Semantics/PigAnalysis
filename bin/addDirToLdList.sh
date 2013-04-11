76fdd437ecd542e27f43431e96337fcac76870b1
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-04-11 17:27:14 +0200

    added script to addd ir to load list

--- bin/addDirToLdList.sh
+++ bin/addDirToLdList.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 if [ -z "$1" ];then
-        echo "at least 1 argument required (directory to remove)"
+        echo "at least 1 argument required (directory to add)"
         exit
 fi
 source /home/OpenPHACTS-Virtuoso/virtuoso-environment.sh;
 isqlFile="${HOME}/.isqlCmdFile.sql"
 dir=$(readlink -f $1)
 
+basename=`basename $dir`
+graphname="http://$basename"
 
-echo "DELETE FROM load_list WHERE ll_file LIKE '$dir*';" > $isqlFile;
+
+echo "ld_dir('$dir','*.nt','$graphname');" > $isqlFile;
 echo "EXIT;" >> $isqlFile;
 echo "" >> $isqlFile;
 cat $isqlFile | isql;
