eef3c47c8c1a73a9527113175e30c4e0e571da47
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-04-20 18:34:16 +0200

    fixed litaslit roundtripping

--- roundtrip/s-o-litAsLit.py
+++ roundtrip/s-o-litAsLit.py
@@ -29,27 +29,35 @@ DEFINE LONGHASH com.data2semantics.pig.udfs.LongHash();
 pigScript += """
 triples = LOAD '$origGraph' USING NtLoader() AS (sub:chararray, pred:chararray, obj:chararray);
 rankedResources = LOAD '$rankingsFile' USING PigStorage() AS (resource:chararray, ranking:double);
+cleanedResources = FOREACH rankedResources {
+	newResource = (resource matches '.*@#@#.*' ? STRSPLIT(resource, '@#@#', 2).$1: resource);
+	
+	
+	---newObj = (newObjTuple.$1 is null? newObjTuple.$0: newObjTuple.$1);
+	----newObjTuple = STRSPLIT(rankedResources.resource, '@#@#', 2);
+	---newObj = (newObjTuple.$1 is null? newObjTuple.$0: newObjTuple.$1);
+	GENERATE newResource AS resource, ranking AS ranking;
+}
 
-subGroup = COGROUP triples by sub, rankedResources by resource;
---- generates: subGroup: {group: chararray,triples: {(sub: chararray,pred: chararray,obj: chararray)},rankedResources: {(resource: chararray,ranking: double)}}
-rankedSubTriples = FOREACH subGroup GENERATE FLATTEN(triples), FLATTEN(rankedResources.ranking) AS subRank;
+subGroup = COGROUP triples by sub, cleanedResources by resource;
+--- generates: subGroup: {group: chararray,triples: {(sub: chararray,pred: chararray,obj: chararray)},cleanedResources: {(resource: chararray,ranking: double)}}
+rankedSubTriples = FOREACH subGroup GENERATE FLATTEN(triples), FLATTEN(cleanedResources.ranking) AS subRank;
 ---generates: rankedSubTriples: {triples::sub: chararray,triples::pred: chararray,triples::obj: chararray,subRank: double}
 
-objGroup = COGROUP rankedSubTriples by obj, rankedResources by resource;
-rankedObjTriples = FOREACH objGroup GENERATE FLATTEN(rankedSubTriples), FLATTEN(rankedResources.ranking) AS objRank;
+objGroup = COGROUP rankedSubTriples by obj, cleanedResources by resource;
+rankedObjTriples = FOREACH objGroup GENERATE FLATTEN(rankedSubTriples), FLATTEN(cleanedResources.ranking) AS objRank;
 
 ---rankedObjTriples: {rankedSubTriples::triples::sub: chararray,rankedSubTriples::triples::pred: chararray,rankedSubTriples::triples::obj: chararray,rankedSubTriples::subRank: double,objRank: double}
-rankedTriples = FOREACH rankedObjTriples {
-		 
-		
-		newObjTuple = STRSPLIT(newObjrankedSubTriples::triples::obj, '@#@#', 2);
-		newObj = (newObjTuple.$1 is null? newObjTuple.$0: newObjTuple.$1);
-		
-		GENERATE rankedSubTriples::triples::sub, rankedSubTriples::triples::pred, newObj, AVG({(rankedSubTriples::subRank is null? 0F: rankedSubTriples::subRank),(objRank is null? 0F: objRank)}) AS ranking;
+rankedTriples = FOREACH rankedObjTriples GENERATE 
+		rankedSubTriples::triples::sub, 
+		rankedSubTriples::triples::pred,
+		rankedSubTriples::triples::obj,
+		AVG({(rankedSubTriples::subRank is null? 0F: rankedSubTriples::subRank),(objRank is null? 0F: objRank)}) AS ranking;
 
 
 rmf $outputFile
 STORE rankedTriples INTO '$outputFile' USING PigStorage();
+
 """
 
 
