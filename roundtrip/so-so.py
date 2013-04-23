ccc84f28b5c377d7a69ff014c0e9b1350e007e9c
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-04-23 20:28:02 +0200

    fixed wrong var issue

--- roundtrip/so-so.py
+++ roundtrip/so-so.py
@@ -37,7 +37,7 @@ cleanedResources = FOREACH rankedResources {
 joinedTriples = JOIN distinctTriples BY (sub, obj), cleanedResources BY ($0, $1);
 
 
-outputGraph = FOREACH filteredTriples GENERATE $0, $1, $2, $5;
+outputGraph = FOREACH joinedTriples GENERATE $0, $1, $2, $5;
 distinctGraph = DISTINCT outputGraph;
 
 rmf $outputFile
