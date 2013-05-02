1d80c1ff655bbefca7657576a09f8ea952a459f2
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-05-02 11:51:22 +0200

    added roudntrip for this rewrite method

---
+++ roundtrip/s-o-litWithPred.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+from org.apache.pig.scripting import Pig
+import sys
+from os.path import dirname, basename, splitext
+
+origGraph = "dbp/dbp.nt"
+rankingsFile = "dbp/analysis/dbp_s-o_unweighted_noLit/directed_indegree"
+outputFile = "dbp/roundtrip/dbp_s-o_unweighted_noLit/directed_indegree"
+
+if (len(sys.argv) < 3):
+	print "takes as argument the analysis file to rewrite, and how to aggregate ('min', 'max', or 'avg'). optional arg: output file"
+	sys.exit(1)
+
+
+rankingsFile = sys.argv[1]
+aggregateMethod = sys.argv[2]
+dataset=rankingsFile.split("/")[0]
+
+origGraph = "%s/%s.nt" % (dataset,dataset)
+outputFile = "%s/roundtrip/%s_%s" % (dataset, basename(rankingsFile), aggregateMethod)
+if len(sys.argv) > 3:
+	outputFile = sys.argv[3]
+	
+pigScript = """
+REGISTER datafu/dist/datafu-0.0.9-SNAPSHOT.jar;
+DEFINE UnorderedPairs datafu.pig.bags.UnorderedPairs();
+REGISTER d2s4pig/target/d2s4pig-1.0.jar
+DEFINE NtLoader com.data2semantics.pig.loaders.NtLoader();
+DEFINE LONGHASH com.data2semantics.pig.udfs.LongHash();
+"""
+
+pigScript += """
+triples = LOAD '$origGraph' USING NtLoader() AS (sub:chararray, pred:chararray, obj:chararray);
+distinctTriples = DISTINCT triples;
+rankedResources = LOAD '$rankingsFile' USING PigStorage() AS (resource:chararray, ranking:double);
+explodedResources = FOREACH rankedResources {
+	newResource = (resource matches '.*@#@#.*' ? STRSPLIT(resource, '@#@#', 2).$1: resource);
+	predicate = (resource matches '.*@#@#.*' ? STRSPLIT(resource, '@#@#', 2).$0: (chararray)null);
+	
+	GENERATE predicate AS predResource, newResource AS resource, ranking AS ranking;
+}
+cleanedResources = DISTINCT explodedResources;
+
+subJoined = JOIN distinctTriples by sub LEFT OUTER, cleanedResources by resource;
+
+
+---first join by both predicate and object, so we get the right literal
+objJoined = JOIN subJoined by ($1, $2) LEFT OUTER, cleanedResources by (predResource, resource);
+
+---then join by regular object
+explodedResourcesWithoutSub = FILTER explodedResources BY predResource is null;
+objJoinedWithRegObjects = JOIN objJoined by $2 LEFT OUTER, explodedResourcesWithoutSub by resource;
+
+---filteredSubJoin = FILTER joinedTriples BY $2 == $4;
+
+
+cleanedBag = FOREACH objJoinedWithRegObjects {
+	subWeight = $5;
+	objWeight = ($8 is null? $11: $8);
+	GENERATE $0 as sub, $1 as pred, $2 as obj, subWeight AS subWeight, objWeight AS objWeight; 
+}
+"""
+
+if aggregateMethod == "avg":
+	pigScript += """
+rankedTriples = FOREACH cleanedBag GENERATE sub, pred, obj, AVG({(subWeight is null? 0F: subWeight),(objWeight is null? 0F: objWeight)}) AS ranking ;"""
+elif aggregateMethod == "max":
+	pigScript += """
+rankedTriples = FOREACH cleanedBag GENERATE sub, pred, obj, MAX({(subWeight is null? 0F: subWeight),(objWeight is null? 0F: objWeight)}) AS ranking ;"""
+elif aggregateMethod == "min":
+	pigScript += """
+rankedTriples = FOREACH cleanedBag GENERATE sub, pred, obj, MIN({(subWeight is null? 0F: subWeight),(objWeight is null? 0F: objWeight)}) AS ranking ;"""
+else: 
+	pigScript += """
+WRONGGGG. how to aggregate?!"""
+
+pigScript += """
+
+
+rmf $outputFile
+STORE rankedTriples INTO '$outputFile' USING PigStorage();
+
+"""
+
+
+P = Pig.compile(pigScript)
+stats = P.bind().runSingle()
+
