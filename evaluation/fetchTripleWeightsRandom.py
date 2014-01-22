b614dbbab27f3d9e6a3d3780466a3d1369cff3c7
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-01-22 18:51:20 +0100

    refactored and removed some code added option to fetch triple weights for random baseline. this method avoids the need to store our random baselines on hdfs, saving disk space (and it does not cost us much computation time)

---
+++ evaluation/fetchTripleWeightsRandom.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+from org.apache.pig.scripting import Pig
+import sys
+from os.path import dirname, basename, splitext
+
+
+if len(sys.argv) < 2:
+    print "at least 2 args required: dataset, and iteration. Optional 3nd arg: query triple file"
+    sys.exit(1)
+
+
+dataset = sys.argv[1]
+
+
+queryTripleFile = "%s/evaluation/qTriples" % (dataset)
+if len(sys.argv) > 3:
+    queryTripleFile = sys.argv[3]
+
+
+outputFile = "%s/evaluation/qTripleWeights/randomSampleBaseline_%s" % (dataset, iteration)
+
+pigScript = """
+REGISTER datafu/dist/datafu-0.0.9-SNAPSHOT.jar;
+DEFINE UnorderedPairs datafu.pig.bags.UnorderedPairs();
+DEFINE MD5 datafu.pig.hash.MD5();
+REGISTER d2s4pig/target/d2s4pig-1.0.jar;
+DEFINE NtLoader com.data2semantics.pig.loaders.NtLoader();
+DEFINE LONGHASH com.data2semantics.pig.udfs.LongHash();
+"""
+
+pigScript += """
+---load qtriples (this load the tuple as 1 pig field. we want to join on the -tuple- anyway)
+qTriples = LOAD '$queryTripleFile' USING TextLoader() AS (triple:chararray);
+origGraph = LOAD '$ntripleFile' USING NtLoader() AS (sub:chararray, pred:chararray, obj:chararray);
+
+graphConcat = FOREACH origGraph GENERATE StringConcat(sub, '\\t', pred, '\\t', obj) AS triple;
+
+randomSampleTriples = FOREACH graphConcat GENERATE triple, RANDOM() AS ranking;
+
+joinedTriples = JOIN qTriples BY $0, randomSampleTriples BY $0;
+
+cleanedResult = FOREACH joinedTriples GENERATE $1, $2;
+
+rmf $outputFile
+STORE cleanedResult INTO '$outputFile' USING PigStorage();
+"""
+
+
+P = Pig.compile(pigScript)
+stats = P.bind().runSingle()
