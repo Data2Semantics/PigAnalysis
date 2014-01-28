09a378f12e56b850baac1411d1a8f9bc29a0cff6
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-01-28 15:54:21 +0100

    added pig script to randomly assign weights to query triples

---
+++ utils/randomlyWeightedQueryTriples.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from org.apache.pig.scripting import Pig
+import sys
+
+if len(sys.argv) <= 2:
+    print "at least 1 arg required (dataset)"
+    sys.exit(1)
+dataset = sys.argv[1]
+inputQtripleFile = "%s/evaluation/qTriples" % (dataset)
+outputFile = "%s/evaluation/randomlyWeightedQtriples"  % (dataset)
+    
+
+pigScript = """
+graph = LOAD '$inputQtripleFile' USING TextLoader() AS (triple:chararray);
+
+
+randomlyWeightedTriples = FOREACH graph GENERATE triple, RANDOM();
+
+
+STORE randomlyWeightedTriples INTO '$outputFile' USING PigStorage();
+"""
+
+P = Pig.compile(pigScript)
+stats = P.bind().runSingle()
