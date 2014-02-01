fb9ed906b31b6892283a481c4049d9540646cb7c
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-02-01 17:57:43 +0100

    added stats script to calc degree distribution

---
+++ stats/calcDegreeDistribution.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+from org.apache.pig.scripting import Pig
+import sys
+
+if len(sys.argv) <= 1:
+    print "at least 1 args required (dataset)"
+    sys.exit(1)
+dataset = sys.argv[1]
+
+indegreeFile = "%s/analysis/resourceSimple_indegree"  % (dataset)
+outdegreeFile = "%s/analysis/resourceSimple_outdegree"  % (dataset)
+
+degreeFile = "%s/evaluation/degree"  % (dataset)
+degreeDistFile = "%s/evaluation/degreeDist"  % (dataset)
+
+pigScript = """
+outdegree = LOAD '$outdegreeFile' USING PigLoader() AS (resource:chararray, ranking:double);
+indegree = LOAD '$indegreeFile' USING PigLoader() AS (resource:chararray, ranking:double);
+
+
+joinedGraph = JOIN outdegree by resource, indegree by resources;
+
+degree = FOREACH joinedGraph GENERATE $0 as resource, SUM($1, $3) AS degreeVal;
+
+STORE degree INTO '$degreeFile' USING PigStorage();
+
+degreeGrouped = GROUP degree BY degreeVal;
+degreeDist = FOREACH degreeGrouped GENERATE group, COUNT(degree);
+
+STORE degreeDist INTO '$degreeDistFile' USING PigStorage();
+"""
+
+P = Pig.compile(pigScript)
+stats = P.bind().runSingle()
